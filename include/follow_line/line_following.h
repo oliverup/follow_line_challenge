d77ce360e4786f1c151b7af68e0abaaf6910c320
Welfengarten <herr.zhangwei_99@outlook.com>
2018-04-25 14:10:25 +0200

    added line following excute function

---
+++ include/follow_line/line_following.h
@@ -0,0 +1,34 @@
+#ifndef FOLLOW_LINE_LINE_FOLLOWING_H
+#define FOLLOW_LINE_LINE_FOLLOWING_H
+
+#include <ros/ros.h>
+#include <luh_youbot_controller_api/controller_api.h>
+#include <geometry_msgs/PoseArray.h>
+#include <tf/transform_listener.h>
+#include <sstream>
+#include <vector>
+
+class LineFollowingNode
+{
+public:
+    LineFollowingNode(ros::NodeHandle &node_handle);
+    ~LineFollowingNode();
+
+    // excute function
+    void LineFollowingExcute(const geometry_msgs::PoseArray pose);
+
+private:
+    // ros node handle
+    ros::NodeHandle *node_;
+
+    // ros subscriber
+    ros::Subscriber line_pose_sub_;
+    // youbot
+    youbot_api::YoubotArm arm_;
+    youbot_api::YoubotBase base_;
+    youbot_api::YoubotGripper gripper_;
+
+    // transform listener
+    tf::TransformListener tf_listener_;    
+};
+#endif // FOLLOW_LINE_LINE_FOLLOWING_H
---
+++ src/line_following.cpp
@@ -0,0 +1,85 @@
+#include "follow_line/line_following.h"
+
+namespace ykin = luh_youbot_kinematics;
+namespace yapi = youbot_api;
+
+//########## CONSTRUCTOR ###############################################################################################
+LineFollowingNode::LineFollowingNode(ros::NodeHandle &node_handle):
+    node_(&node_handle)
+{
+    // === INIT YOUBOT ===
+    arm_.init(node_handle);
+    base_.init(node_handle);
+    gripper_.init(node_handle);
+
+    line_pose_sub_ = node_->subscribe("line_following_coordinates", 1000, &LineFollowingNode::LineFollowingExcute, this);
+}
+
+//########## DESTRUCTOR ################################################################################################
+LineFollowingNode::~LineFollowingNode()
+{
+
+}
+
+//########## ACTION SERVER EXCUTE ######################################################################################
+void LineFollowingNode::LineFollowingExcute(const geometry_msgs::PoseArray goal)
+{
+    // Wait for transform
+    bool transform_is_available = tf_listener_.waitForTransform("arm_link_0", goal.header.frame_id,
+                                                                goal.header.stamp, ros::Duration(2.0));
+
+
+    // Check, if transform is available
+    if(!transform_is_available) {
+        ROS_ERROR("Transform from %s to arm_link_0 is not available.", goal.header.frame_id.c_str());
+        return;
+    }
+    else {
+        ROS_INFO("Transform from %s to arm_link_0.", goal.header.frame_id.c_str());
+    }
+
+    // Transform the pose to arm_link_0
+    geometry_msgs::PoseStamped pose_in;
+    geometry_msgs::PoseStamped pose_out;
+    std::vector<ykin::CartesianPosition> cartesian_path;
+    cartesian_path.resize(goal.poses.size());
+
+    for(int i = 0; i < goal.poses.size(); i++) {
+      pose_in.pose = goal.poses[i];
+      pose_in.header = goal.header;
+
+      try {
+          tf_listener_.transformPose("arm_link_0", pose_in, pose_out);
+      }
+      catch(tf::TransformException ex) {
+          ROS_ERROR("Transform error: %s at item: %d", ex.what(), i);
+          return;
+      }
+
+      cartesian_path[i].setX(pose_out.pose.position.x);
+      cartesian_path[i].setY(pose_out.pose.position.y);
+      cartesian_path[i].setZ(pose_out.pose.position.z);
+      // Theta = k1 * position.x + b1
+      cartesian_path[i].setTheta(-(5.0/3.0)*pose_in.pose.position.x*M_PI +11.0/6.0*M_PI);
+      cartesian_path[i].setQ5(0.0);
+    }
+
+    // === DO ARM MOVEMENT ===
+    // Move the arm alone a path
+    arm_.moveAlongPath(cartesian_path);
+    arm_.waitForCurrentAction();
+}
+
+//########## MAIN ######################################################################################################
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "line_following");
+
+    ros::NodeHandle node_handle;
+    LineFollowingNode line_following(node_handle);
+
+    ROS_INFO("Node is spinning...");
+    ros::spin();
+
+    return 0;
+}
